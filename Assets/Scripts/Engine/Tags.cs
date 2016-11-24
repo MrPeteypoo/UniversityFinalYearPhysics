d08168aeb65bcd415a5e04cf3c1bd17ac162f13f
Simon Peter Campbell <storm20200@gmail.com>
2016-11-24 07:34:04 +0000

    Remove unnecessary assemblies from Tags.

--- Assets/Scripts/Engine/Tags.cs
+++ Assets/Scripts/Engine/Tags.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-
+﻿/// <summary>
+/// A collection of every tag used in the engine.
+/// </summary>
 public static class Tags
 {
     public const string engine = "Engine";
