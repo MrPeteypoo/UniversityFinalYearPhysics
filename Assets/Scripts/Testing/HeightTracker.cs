e14efe00bfdf96ff66c68c4c56c3adf94db7c398
Simon Peter Campbell <storm20200@gmail.com>
2016-11-29 08:51:15 +0000

    Added a height tracker for testing.

---
+++ Assets/Scripts/Testing/HeightTracker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightTracker : MonoBehaviour {
+    float m_time = 0f;
+	// Use this for initialization
+	void Start () {
+        Debug.Log ("Starting height: " + transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (m_time >= 1f)
+        {
+            Debug.Log ("Height: " + transform.position.y);
+            m_time -= 1f;
+        }
+        m_time += Time.deltaTime;
+	}
+}
